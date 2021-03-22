d361e9e1dfd4d63130a0ad33f1ad682f3c394f2f
Buck Turk <37983248+thecolonel63@users.noreply.github.com>
2021-03-21 21:38:54 -0500

    Add files via upload
    Fixed issue with port always being 25565

--- working_dir/address.bat
+++ working_dir/address.bat
@@ -2,7 +2,7 @@
 cls
 set /p addr="Enter Address: "
 set /p port="Enter Port (Leave blank for 25565): "
-if not defined %%port%% set port=25565
+if not defined port set port=25565
 cd %cd%\working_dir\
 type %cd%\text\text1.txt>%cd%\ping\view_serverping.php
 echo %addr%' ); >> %cd%\ping\view_serverping.php
